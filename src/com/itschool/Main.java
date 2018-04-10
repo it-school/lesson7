eb3c849c47ab0131bb29f35aadc0b53b7ad27990
IT-school.com.ua <37939704+it-school@users.noreply.github.com>
2018-04-10 21:21:42 +0300

    Update Main.java

--- src/com/itschool/Main.java
+++ src/com/itschool/Main.java
@@ -3,11 +3,11 @@ package com.itschool;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //System.out.println(Factorial.factorialR(5));
-        System.out.println(Factorial.factorial(5));
+        System.out.println(Factorial.factorial(5));  // вызов статического метода (вычисление без использования рекурсии)
 
+	//System.out.println(Factorial.factorialR(5));  // вызов нестатического метода класса без создания объекта запрещён
+	    
         Factorial fact = new Factorial();
-        System.out.println(fact.factorialR(5));
+        System.out.println(fact.factorialR(5));  // вызов нестатического метода (вычисление с использованием рекурсии)
     }
 }
